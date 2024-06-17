b58166d192915cc159ca431ba10fadd2d8cf915a
ackimixs <ackimixs@protonmail.com>
2024-06-17 15:37:16 +0200

    functionnalité 3

--- f3.py
+++ f3.py
@@ -13,33 +13,28 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
 
+
 parser = CLParser(sys.argv)
-file = parser.get_one_option("file","Data_Arbre.csv")
+file = parser.get_one_option("file", "Data_Arbre.csv")
 
-X_to_keep = parser.get_option("X", ["haut_tronc", "haut_tot", "tronc_diam"])
-Y_to_keep = parser.get_one_option("Y", "age_estim")
+X_to_keep = parser.get_option("X", ["haut_tronc"])
+Y_to_keep = parser.get_one_option("Y", "fk_arb_etat")
 
-# print(file, X_to_keep, Y_to_keep)
+
+def parse_arb_etat(x):
+    if x == "Essouché" or x == "Non essouché":
+        return 1
+    return 0
+
+
+print(file, X_to_keep, Y_to_keep)
 
 data = pd.read_csv(file)
 
 X = data[X_to_keep]
 Y = data[Y_to_keep]
 
-
-# print(X.shape, Y.shape)
-
-def parse_value(x):
-    if x < 90:
-        return int(x / 10)
-    return 9
-
-
-Y = Y.apply(parse_value)
-
-# print(Y.head())
-
-# print(Y.value_counts())
+Y = Y.apply(parse_arb_etat)
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 
