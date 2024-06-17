c9dd504a09470e57d23b043c0b27dd9e6536b180
ackimixs <ackimixs@protonmail.com>
2024-06-17 14:23:23 +0200

    ZOU

--- CLParser.py
+++ CLParser.py
@@ -4,17 +4,27 @@ class CLParser:
         self._argc = len(argv)
         self._options = {}
 
-        for i in range(1, self._argc):
+        i = 1
+        while i < self._argc:
             if self._argv[i].startswith("--"):
                 option = self._argv[i][2:]
-                if i + 1 < self._argc and not self._argv[i + 1].startswith("--"):
-                    self._options[option] = self._argv[i + 1]
-                else:
-                    self._options[option] = ""
+                values = []
+                i += 1
+                while i < self._argc and not self._argv[i].startswith("--"):
+                    values.append(self._argv[i])
+                    i += 1
+                self._options[option] = values if values else [""]
+            else:
+                i += 1
 
-    def get_option(self, option, default_value=None):
+    def get_option(self, option, default_value=None) -> list:
         if option in self._options:
             return self._options[option]
+        return default_value
+
+    def get_one_option(self, option, default_value=None) -> str:
+        if option in self._options:
+            return self._options[option][0]
         return default_value
 
     def has_option(self, option):
@@ -30,30 +40,3 @@ class CLParser:
 
     def positional_arguments_count(self):
         return self._argc
-
-    def parse_string(self, str_val, type_):
-        try:
-            if type_ == 'STRING':
-                return str_val
-            elif type_ == 'INT':
-                return int(str_val)
-            elif type_ == 'FLOAT':
-                return float(str_val)
-            elif type_ == 'DOUBLE':
-                return float(str_val)  # Python's float is double precision
-            elif type_ == 'BOOL':
-                return str_val.lower() == "true"
-            elif type_ == 'LONG':
-                return int(str_val)
-            elif type_ == 'SHORT':
-                return int(str_val)  # Python does not have short, so use int
-            elif type_ == 'CHAR':
-                return str_val[0]
-            elif type_ == 'CHAR_PTR':
-                return str_val
-            elif type_ == 'CONST_CHAR_PTR':
-                return str_val
-            else:
-                raise ValueError("Invalid type")
-        except ValueError:
-            raise ValueError(f"Invalid value for value: {str_val}")
---
+++ f2.py
@@ -0,0 +1,49 @@
+import pandas as pd
+from CLParser import CLParser
+import sys
+from sklearn.linear_model import SGDClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+
+parser = CLParser(sys.argv)
+file = parser.get_option("file", "Data_Arbre.csv")
+
+X_to_keep = parser.get_option("X", ["haut_tronc", "haut_tot", "tronc_diam"])
+Y_to_keep = parser.get_one_option("Y", "age_estim")
+
+print(file, X_to_keep, Y_to_keep)
+
+data = pd.read_csv(file)
+
+X = data[X_to_keep]
+Y = data[Y_to_keep]
+
+# print(X.shape, Y.shape)
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+
+# print(X_train.shape, X_test.shape, Y_train.shape, Y_test.shape)
+
+sgd_clf = SGDClassifier().fit(X_train, Y_train)
+
+res = cross_val_score(sgd_clf, X_train, Y_train, scoring="accuracy", cv=3)
+
+print(res)
+print(res.mean())
+
+rdf = RandomForestClassifier().fit(X_train, Y_train)
+
+res = cross_val_score(rdf, X_train, Y_train, scoring="accuracy", cv=3)
+
+print(res)
+print(res.mean())
+
+neural = MLPClassifier().fit(X_train, Y_train)
+
+res = cross_val_score(neural, X_train, Y_train, scoring="accuracy", cv=3)
+
+print(res)
+print(res.mean())
