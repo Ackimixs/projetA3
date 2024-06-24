32d7e9e8ca18dd9c7b15c1925b779240b8fd7ffc
ackimixs <ackimixs@protonmail.com>
2024-06-24 11:50:51 +0200

    init

--- backend/database/hidden.constants.php
+++ backend/database/hidden.constants.php
@@ -1,7 +1,7 @@
 <?php
 // Database constants.
-const DB_USER = 'spotifaie_user';
-const DB_PASSWORD = 'password';
-const DB_NAME = 'spotifaie';
+const DB_USER = 'tree';
+const DB_PASSWORD = 'tree';
+const DB_NAME = 'tree';
 const DB_SERVER = 'localhost';
 const DB_PORT = '5432';
