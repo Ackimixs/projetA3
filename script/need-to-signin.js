55e720385ed776f0baf6cc93b562cda1c225e144
acki <ackimixs@protonmail.com>
2024-06-25 12:30:23 +0200

    message

--- script/need-to-signin.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('pageshow', () => {
-    if (!window.location.pathname.includes('auth')) {
-        if (localStorage.getItem('user') === null) {
-            window.location.href = `/auth.html?redirect=${encodeURIComponent(window.location.pathname)}`;
-        }
-    }
-});
